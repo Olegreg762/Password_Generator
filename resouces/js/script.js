7a133691c79674463e11146e6a6f943b128d231a
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-06-20 10:48:03 -0500

    begin writing password generate function

--- resouces/js/script.js
+++ resouces/js/script.js
@@ -1,5 +1,39 @@
 // Assignment code here
 
+function number_generator(min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generatePassword(length, inlcude_upper_case, inlcude_lower_case, inlcude_special_chars, inlcude_numbers){
+  let included_chars = "";
+  const all_chars = number_generator(33,127);
+  const upper_case = number_generator(65,90);
+  const lower_case = number_generator(97,122);
+  const special_chars = number_generator(33,64);
+  const numbers = number_generator(48,57);
+
+  if (inlcude_upper_case, inlcude_lower_case, inlcude_special_chars, inlcude_numbers){
+    included_chars = all_chars 
+  }
+
+  if (inlcude_upper_case){
+    included_chars += upper_case;
+  }
+
+  if (inlcude_lower_case){
+    included_chars += lower_case;
+  }
+
+  if (inlcude_special_chars){
+    included_chars += special_chars;
+  }
+
+  if(inlcude_numbers){
+    included_chars += numbers;
+  }
+
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
