3f2bf1ee371d4196d45953c0e5bda959b13f60a6
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-06-21 22:01:11 -0500

    updated to css for dark theme and script.js update

--- resouces/js/script.js
+++ resouces/js/script.js
@@ -1,25 +1,45 @@
 // Assignment code here
 
-
+function sercure_alert(){
+  let confrimation = confirm("Your Password Will Be Less Secure!\nPress OK to Include and Cancel to Continue With Less Secure Password");
+    return confrimation
+}
 
 function generatePassword(){
 
   let length = prompt("How long would you like your Password?\nChoose Between 8 and 128 characters");
-  if(length <=8 || length >=128)length = prompt("Please Choose between 8 and 128 characters");
-  if(!Number.isInteger(length))length= prompt("Please Choose between 8 and 128 characters") ;
+  while(isNaN(length) || length < 8 || length > 128){
+    length = prompt("Please Choose between 8 and 128 characters")
+  }
 
   let include_upper_case = confirm("Would you like to include Upper Case Letters?");
-  if (!include_upper_case) include_upper_case = confirm("Your Password will be less secure!\nPress OK to include and Cancel to Continue less Secure Password");
+  if (!include_upper_case) include_upper_case = sercure_alert()
 
   let include_lower_case = confirm("Would you like to include Lower Case Letters?");
-  if (!include_lower_case) include_lower_case = confirm("Your Password will be less secure!\nPress OK to include and Cancel to Continue less Secure Password");
-  
+  if (!include_lower_case) include_lower_case = sercure_alert()
+
   let include_special_chars = confirm("Would you like Special Characters?");
-  if (!include_special_chars) include_special_chars = confirm("Your Password will be less secure!\nPress OK to include and Cancel to Continue less Secure Password");
-  
+  if (!include_special_chars) include_special_chars = sercure_alert()
+
   let include_numbers = confirm("Do you want Numbers in included?");
-  if (!include_numbers) include_numbers = confirm("Your Password will be less secure!\nPress OK to include and Cancel to Continue less Secure Password");
+  if (!include_numbers) include_numbers = sercure_alert()
+
+  while(!include_upper_case && !include_lower_case && !include_special_chars && !include_numbers){
+    alert("You Must Choose At Least One Group To Be Included")
+
+    include_upper_case = confirm("Would you like to include Upper Case Letters?");
+    if (!include_upper_case) include_upper_case = sercure_alert()
   
+    include_lower_case = confirm("Would you like to include Lower Case Letters?");
+    if (!include_lower_case) include_lower_case = sercure_alert()
+  
+    include_special_chars = confirm("Would you like Special Characters?");
+    if (!include_special_chars) include_special_chars = sercure_alert()
+  
+    include_numbers = confirm("Do you want Numbers in included?");
+    if (!include_numbers) include_numbers = sercure_alert()
+  }
+
   let included_chars = "";
   let password = "";
 
