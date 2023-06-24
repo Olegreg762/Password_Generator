c53ab57c52a378839357916f9fc049ae5846eaf3
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-06-24 05:14:11 -0500

    scriptOptimize, input css updates, html id updates

--- resouces/js/script.js
+++ resouces/js/script.js
@@ -1,30 +1,33 @@
 // Assignment code here
 
-function confirm_include(id, group){
-  let include_confirm = document.getElementById(id).checked;
-  if (!include_confirm) include_confirm = confirm("Your Password Will Be Less Secure!\nPress OK to Include " 
-  + group +
-  "\nPress Cancel to Continue With A Less Secure Password With\n" 
-  + group + " Not Included.");
+// Function that sets the characters to be included in password
+function confirm_include(group){
+  // Looks at HTML doc to determine if checkbox is checked or not 
+  let include_confirm = document.getElementById(group).checked;
+  // IF not checked displays warning about password being less secure
+  if (!include_confirm){ include_confirm = confirm("Your Password Will Be Less Secure!\nPress OK to Include " 
+  + group + "!"+
+  "\nPress Cancel to Continue With A Less Secure Password!\n");
+  // Forces checkbox to be checked if user decides to include character group
+  document.getElementById(group).checked = include_confirm;
+}
 
     return include_confirm;
 }
-
+// Function that generates password
 function generatePassword(){
-
+  // Allows user to determine lenght of password between 8 and 128 digits
   let length = document.getElementById("length").value;
+  // Displays warning if length not between 8-128 and/or Not a Number
   if(isNaN(length) || length < 8 || length > 128){
     alert("Length of Pasword Must Be a Number Between 8 and 128");
     length = 0;
   }
-
-  let include_upper_case = confirm_include("upper", "Upper Case Letters")
-
-  let include_lower_case = confirm_include("lower", "Lower Case Letters");
-
-  let include_special_chars = confirm_include("special", "Special Character");
-
-  let include_numbers = confirm_include("number", "Numbers");
+  // Defines Characters groups
+  let include_upper_case = confirm_include("Upper Case Letters");
+  let include_lower_case = confirm_include("Lower Case Letters");
+  let include_special_chars = confirm_include("Special Character");
+  let include_numbers = confirm_include("Numbers");
   
   
   let included_chars = "";
